0c1f49b8dc483b625c30fce0c42558c60772fed7
rohitdr786 <rohitdr786@gmail.com>
2018-01-25 18:20:43 -0500

    'React:Session_1:Intro_to_State'

---
+++ src/components/search_bar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'; // we need to import react because
+// the JSX tag is converted to javascript like React.createlement
+// and if React is not defined it will throw an error.
+
+// class way of defining.
+
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { term : '' };
+  }
+
+  render() {
+
+    // further minimized code
+    return (
+      <div>
+        <input onChange={ event => this.setState({ term : event.target.value}) } />
+      //  Value in text box is {this.state.term}
+      </div>
+    );
+  }
+
+  // handleInputChange(event) {
+  //   console.log(event.target);
+  // }
+
+}
+
+// Functional wah of defining. Commented as we switched to class way
+// of defining as we need to maintain the user values
+
+// const SearchBar = () => { // short form of function.
+//   // Also has different value for this keyword when used.
+//   return <input />;
+// }
+
+export default SearchBar; // export to make it available in the
+// files that import this file. When imported the naming has to
+// be same as the eported name unless we use the default keyword.
+// If a default value is import we can use a different name.
+// Only one export can have default.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar' // since its not a lib file
+// we have to specify the path. Also we can use a different name in place of
+// SearchBar as its a default export.
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyB2exktkamhieGx0RxKOOR14-tHMLlkbhU';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a component and this component should produce an HTML
+const App = function () {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+// Take this components HTML and put it on the page(ie. in the DOM)
+ReactDOM.render(<App />,document.querySelector('.container'));
